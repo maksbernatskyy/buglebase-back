8d4c442d708f5af9aea3004515e2904324a3836a
maksbernatskyy <maksbernatskyy@gmail.com>
2026-04-07 14:13:32 +0200

    add delete

--- src/main/java/org/lessons/java/games/buglebase_back/controller/GameRestController.java
+++ src/main/java/org/lessons/java/games/buglebase_back/controller/GameRestController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -58,5 +59,10 @@ public class GameRestController {
         Game game = gameService.saveGame(formGame);
         game.setId(id);
         return new ResponseEntity<Game>(game, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}") 
+    public void delete(@PathVariable("id") Integer id) {
+        gameService.deleteGameById(id);
     }
 }
