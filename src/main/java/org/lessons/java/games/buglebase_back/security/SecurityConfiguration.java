073385c2bccc7a7cf35d61f24009b5ff610bc480
maksbernatskyy <maksbernatskyy@gmail.com>
2026-04-08 14:14:35 +0200

    add SecurityConfiguration

---
+++ src/main/java/org/lessons/java/games/buglebase_back/security/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package org.lessons.java.games.buglebase_back.security;
+
+import org.lessons.java.games.buglebase_back.service.DatabaseUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/user").hasAuthority("USER")
+                .requestMatchers("/admin").hasAuthority("ADMIN")
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/**").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    DatabaseUserDetailsService userDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
