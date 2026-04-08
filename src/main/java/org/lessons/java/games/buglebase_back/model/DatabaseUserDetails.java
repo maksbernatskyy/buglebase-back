ce0794017896920e6528894d4a24762427902fb2
maksbernatskyy <maksbernatskyy@gmail.com>
2026-04-08 12:52:57 +0200

    add folder security

--- src/main/java/org/lessons/java/games/buglebase_back/model/DatabaseUserDetails.java
+++
@@ -1,7 +0,0 @@
-package org.lessons.java.games.buglebase_back.model;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public class DatabaseUserDetails implements UserDetails{
-    
-}
